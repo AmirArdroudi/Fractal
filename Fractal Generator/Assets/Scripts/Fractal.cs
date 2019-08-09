1cf4f84731e7c863896d7c5c8b221358c00770c7
Coda <a.ardroudi@gmail.com>
2019-08-09 21:26:56 +0430

    fractal update

--- Fractal Generator/Assets/Scripts/Fractal.cs
+++ Fractal Generator/Assets/Scripts/Fractal.cs
@@ -1,18 +1,47 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Fractal : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public Mesh mesh;
+    public Material material;
+
+    public int maxDepth;
+    public float childScale;
+    private int depth;
+
+
     void Start()
     {
+        gameObject.AddComponent<MeshFilter>().mesh = mesh;
+        gameObject.AddComponent<MeshRenderer>().material = material;
+        if(depth < maxDepth)
+        {
+            StartCoroutine(CreateChildren());            
+        }
+    }
+
+    private IEnumerator CreateChildren()
+    {
+        yield return new WaitForSeconds(0.5f);
+        new GameObject("Fractal Child").AddComponent<Fractal>().Initialize(this, Vector3.left);
+        yield return new WaitForSeconds(0.5f);
+        new GameObject("Fractal Child").AddComponent<Fractal>().Initialize(this, Vector3.right);
+        yield return new WaitForSeconds(0.5f);
+        new GameObject("Fractal Child").AddComponent<Fractal>().Initialize(this, Vector3.up);
+    }
+
+    private void Initialize(Fractal parent, Vector3 direction)
+    {
+        mesh = parent.mesh;
+        material = parent.material;
+        maxDepth = parent.maxDepth;
+        depth = parent.depth + 1;
+        childScale = parent.childScale;
+        transform.parent = parent.transform;
+        transform.localScale = Vector3.one * childScale;
+        transform.localPosition = direction * (.5f + .5f * childScale);
         
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
